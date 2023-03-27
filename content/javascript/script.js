b7b8c65df55ab573508976cd35c366e61be8a66f
masonharniess <masonharniess@outlook.com>
2023-03-27 13:28:10 +0100

    basic project set up

---
+++ content/javascript/script.js
@@ -0,0 +1,8 @@
+function displayHamburger() {
+    const toggle = document.querySelector('.toggle');
+    const menu = document.querySelector('.menu');
+
+    toggle.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+}
