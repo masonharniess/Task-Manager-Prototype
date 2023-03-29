55aa62e854ee6bf9661a57a11b5fd15478af4b64
masonharniess <masonharniess@outlook.com>
2023-03-29 03:47:27 +0100

    search bar: border radius now changes if there are suggestions visible

--- content/javascript/search.js
+++ content/javascript/search.js
@@ -11,15 +11,19 @@ function autocomplete(inp, arr) {
         currentFocus = -1;
         /*create a DIV element that will contain the items (values):*/
         a = document.createElement("DIV");
+        // document.getElementById("input-searchID").style.color = "red";
         a.setAttribute("id", this.id + "autocomplete-list");
         a.setAttribute("class", "autocomplete-items");
         /*append the DIV element as a child of the autocomplete container:*/
         this.parentNode.appendChild(a);
         /*for each item in the array...*/
         for (i = 0; i < arr.length; i++) {
+
             /*check if the item starts with the same letters as the text field value:*/
             if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
                 /*create a DIV element for each matching element:*/
+                document.getElementById("input-searchID").style.borderBottomRightRadius = "0";
+                document.getElementById("input-searchID").style.borderBottomLeftRadius = "0";
                 b = document.createElement("DIV");
                 /*make the matching letters bold:*/
                 b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
@@ -41,18 +45,21 @@ function autocomplete(inp, arr) {
     /*execute a function presses a key on the keyboard:*/
     inp.addEventListener("keydown", function(e) {
         var x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
+        if (x) {
+            x = x.getElementsByTagName("div");
+        }
         if (e.keyCode == 40) {
             /*If the arrow DOWN key is pressed,
             increase the currentFocus variable:*/
             currentFocus++;
-            /*and and make the current item more visible:*/
+            /*and make the current item more visible:*/
             addActive(x);
         } else if (e.keyCode == 38) { //up
             /*If the arrow UP key is pressed,
             decrease the currentFocus variable:*/
             currentFocus--;
-            /*and and make the current item more visible:*/
+
+            /*and make the current item more visible:*/
             addActive(x);
         } else if (e.keyCode == 13) {
             /*If the ENTER key is pressed, prevent the form from being submitted,*/
@@ -65,7 +72,9 @@ function autocomplete(inp, arr) {
     });
     function addActive(x) {
         /*a function to classify an item as "active":*/
-        if (!x) return false;
+        if (!x) {
+            return false;
+        }
         /*start by removing the "active" class on all items:*/
         removeActive(x);
         if (currentFocus >= x.length) currentFocus = 0;
@@ -76,6 +85,7 @@ function autocomplete(inp, arr) {
     function removeActive(x) {
         /*a function to remove the "active" class from all autocomplete items:*/
         for (var i = 0; i < x.length; i++) {
+
             x[i].classList.remove("autocomplete-active");
         }
     }
@@ -84,6 +94,9 @@ function autocomplete(inp, arr) {
         except the one passed as an argument:*/
         var x = document.getElementsByClassName("autocomplete-items");
         for (var i = 0; i < x.length; i++) {
+
+            document.getElementById("input-searchID").style.borderBottomRightRadius = "25px";
+            document.getElementById("input-searchID").style.borderBottomLeftRadius = "25px";
             if (elmnt != x[i] && elmnt != inp) {
                 x[i].parentNode.removeChild(x[i]);
             }
