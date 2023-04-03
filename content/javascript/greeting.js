88a16a79f210822bee96106f84a48c51ffb03b8a
masonharniess <masonharniess@outlook.com>
2023-04-03 17:33:42 +0100

    greeting now time specific

---
+++ content/javascript/greeting.js
@@ -0,0 +1,13 @@
+function greeting (){
+    const now = new Date();
+    const currentHour = now.getHours();
+    const greetingDiv = document.getElementById("greeting");
+
+    if (currentHour < 12) {
+        greetingDiv.textContent = "Good morning, Mason";
+    } else if (currentHour < 18) {
+        greetingDiv.textContent = "Good afternoon, Mason";
+    } else {
+        greetingDiv.textContent = "Good evening Mason";
+    }
+}
