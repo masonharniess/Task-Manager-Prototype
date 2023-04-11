e6c1f20415ca4ee00691bb8e4b06520c0d92c208
masonharniess <masonharniess@outlook.com>
2023-04-11 13:50:40 +0100

    dropdown for new divs works

--- content/javascript/dropdown.js
+++ content/javascript/dropdown.js
@@ -1,23 +1,45 @@
-function dropDownMenu() {
-    var acc = document.getElementsByClassName("wrapper");
-    var i;
+// function dropDownMenu() {
+//     var acc = document.getElementsByClassName("wrapper");
+//     var i;
+//
+//     for (i = 0; i < acc.length; i++) {
+//         acc[i].addEventListener("click", function() {
+//             this.classList.toggle("active");
+//             var panel = this.nextElementSibling;
+//             if (panel.style.maxHeight) {
+//                 panel.style.maxHeight = null;
+//             } else {
+//                 panel.style.maxHeight = 35 + "px";
+//             }
+//         });
+//
+//         acc[i].addEventListener("keydown", function(event) {
+//             if (event.key === "Enter") {
+//                 event.preventDefault();
+//                 this.click();
+//             }
+//         });
+//     }
+// }
 
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
+function dropDownMenu() {
+    document.addEventListener('click', function(event) {
+        var wrapper = event.target.closest('.wrapper');
+        if (wrapper) {
+            wrapper.classList.toggle('active');
+            var panel = wrapper.nextElementSibling;
             if (panel.style.maxHeight) {
                 panel.style.maxHeight = null;
             } else {
-                panel.style.maxHeight = 35 + "px";
+                panel.style.maxHeight = panel.scrollHeight + '35px';
             }
-        });
+        }
+    });
 
-        acc[i].addEventListener("keydown", function(event) {
-            if (event.key === "Enter") {
-                event.preventDefault();
-                this.click();
-            }
-        });
-    }
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter' && event.target.classList.contains('wrapper')) {
+            event.preventDefault();
+            event.target.click();
+        }
+    });
 }
