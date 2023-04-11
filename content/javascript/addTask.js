1e0476f480bfe470a83b313dd158426cd3a93959
masonharniess <masonharniess@outlook.com>
2023-04-11 20:36:43 +0100

    random changes

---
+++ content/javascript/addTask.js
@@ -0,0 +1,18 @@
+function addTask() {
+    var newDiv = document.createElement("div");
+    newDiv.className ="task-block";
+    newDiv.innerHTML =
+        "                <div class=\"wrapper\">\n" +
+        "                    <li tabindex=0 class=\"recent-tasks\">\n" +
+        "                        <div class=\"content-title text-highlight\">\n" +
+        "                            New Task Title\n" +
+        "                            <span class=\"arrow\"></span>\n" +
+        "                        </div>\n" +
+        "                    </li>\n" +
+        "                </div>\n" +
+        "                <div class=\"sub-content\">\n" +
+        "                    New task description\n" +
+        "                </div>\n"
+    var container = document.getElementById("tasks");
+    container.appendChild(newDiv);
+}
