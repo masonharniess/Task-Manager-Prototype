3eac9a87be3e30ff6c858ea1fed5d2333ed6f806
masonharniess <masonharniess@outlook.com>
2023-04-13 12:45:45 +0100

    task delete works on all non-dynamically created tasks

--- content/javascript/addTask.js
+++ content/javascript/addTask.js
@@ -1,18 +1,19 @@
 function addTask() {
     var newDiv = document.createElement("div");
     newDiv.className ="task-block";
-    newDiv.innerHTML =
-        "                <div class=\"wrapper\">\n" +
-        "                    <li tabindex=0 class=\"recent-tasks\">\n" +
-        "                        <div class=\"content-title text-highlight\">\n" +
-        "                            New Task Title\n" +
-        "                            <span class=\"arrow\"></span>\n" +
-        "                        </div>\n" +
-        "                    </li>\n" +
-        "                </div>\n" +
-        "                <div class=\"sub-content\">\n" +
-        "                    New task description\n" +
-        "                </div>\n"
+    newDiv.innerHTML = "<div class=\"wrapper\">\n" +
+        "                        <li tabindex=0 class=\"recent-tasks\">\n" +
+        "                            <div class=\"content-title text-highlight\">\n" +
+        "                                New Task Title\n" +
+        "\n" +
+        "                                <i onclick=\"deleteDiv()\" class=\"fa-solid fa-trash-can deleteBtn\"></i>\n" +
+        "                                <span class=\"arrow\"></span>\n" +
+        "                            </div>\n" +
+        "                        </li>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"sub-content\">\n" +
+        "                        New Task Description\n" +
+        "                    </div>"
     var container = document.getElementById("tasks");
     container.appendChild(newDiv);
 }
