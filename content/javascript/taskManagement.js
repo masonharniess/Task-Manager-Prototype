e09273c3ced2c26bf9e8db6d069387c0224f2be8
masonharniess <masonharniess@outlook.com>
2023-04-14 14:30:49 +0100

    random

--- content/javascript/taskManagement.js
+++ content/javascript/taskManagement.js
@@ -8,13 +8,15 @@ function addTask() {
         "                                    New Task Title\n" +
         "                                </span>" +
         "\n" +
-        "                                <i class=\"fa-solid fa-trash-can deleteBtn\"></i>\n <i class=\"fa-solid fa-pen-to-square\"></i>" +
+        "                                <i class=\"fa-solid fa-trash-can deleteBtn\"></i>\n" +
         "                                <span class=\"arrow\"></span>\n" +
         "                            </div>\n" +
         "                        </li>\n" +
         "                    </div>\n" +
         "                    <div class=\"sub-content editableDiv\" >\n" +
-        "                        New Task Description\n" +
+        "                                                <p>\n" +
+        "                            New task description\n" +
+        "                        </p>" +
         "                    </div>"
     var container = document.getElementById("tasks");
     container.appendChild(newDiv);
@@ -24,21 +26,51 @@ function deleteTask() {
     var container = document.getElementById("tasks"); // Find the container element
 
     container.addEventListener("click", function (event) {
-        if (event.target.matches(".deleteBtn")) { // Check if the clicked element matches the delete button selector
-            var div = event.target.closest(".task-block"); // Find the div that contains the clicked button
-            div.remove(); // Remove the div from the HTML document
+        if (event.target.matches(".deleteBtn")) {
+            var div = event.target.closest(".task-block");
+            div.remove();
         }
     });
 }
+
+// function toggleContentEditable() {
+//     var divs = document.querySelectorAll(".editableDiv");
+//     for (var i = 0; i < divs.length; i++) {
+//         var div = divs[i];
+//         if (div.getAttribute("contenteditable") === "true") {
+//             div.setAttribute("contenteditable", "false");
+//         } else {
+//             div.setAttribute("contenteditable", "true");
+//         }
+//     }
+// }
 
 function toggleContentEditable() {
     var divs = document.querySelectorAll(".editableDiv");
     for (var i = 0; i < divs.length; i++) {
         var div = divs[i];
-        if (div.getAttribute("contenteditable") === "true") {
-            div.setAttribute("contenteditable", "false");
-        } else {
-            div.setAttribute("contenteditable", "true");
-        }
+        var isEditable = div.getAttribute("contenteditable") === "true";
+        div.setAttribute("contenteditable", isEditable ? "false" : "true");
     }
+
+    window.addEventListener('DOMContentLoaded', function() {
+        var divs = document.querySelectorAll(".editableDiv");
+        for (var i = 0; i < divs.length; i++) {
+            divs[i].setAttribute("contenteditable", "false");
+        }
+    });
+}
+
+
+
+
+function changeEditColour() {
+    const myButton = document.getElementById("editTasksButton");
+    myButton.addEventListener("click", function () {
+        if (myButton.classList.contains("edit-task")) {
+            myButton.classList.remove("edit-task");
+        } else {
+            myButton.classList.add("edit-task");
+        }
+    });
 }
