b3cb885732168fbb8afc358273677f8b6aaaace0
masonharniess <masonharniess@outlook.com>
2023-04-19 18:39:39 +0100

    navigation sub menus working

---
+++ updatedContent/updatedJS/updatedGeneral/navigationBar/settingsToggles/toggleData.js
@@ -0,0 +1,19 @@
+function toggleDark() {
+    var checkbox = document.getElementById("switch_checkbox_dark");
+    checkbox.checked = !checkbox.checked;
+}
+
+function toggleData() {
+    var checkbox = document.getElementById("switch_checkbox_share");
+    checkbox.checked = !checkbox.checked;
+}
+
+function toggleFilter() {
+    var checkbox = document.getElementById("switch_checkbox_filter");
+    checkbox.checked = !checkbox.checked;
+}
+
+function toggleDisturb() {
+    var checkbox = document.getElementById("switch_checkbox_disturb");
+    checkbox.checked = !checkbox.checked;
+}
