1a32ec7dc34edf0a57d7d0260ed4f1b3d0004f76
masonharniess <masonharniess@outlook.com>
2023-04-19 17:46:57 +0100

    notifications pop up kinda working

--- updatedContent/updatedJS/updatedGeneral/toggleAccountMenu.js
+++ updatedContent/updatedJS/updatedGeneral/toggleAccountMenu.js
@@ -1,4 +1,12 @@
 function toggleAccountMenu() {
     let submenu = document.getElementById("id_account_submenu_wrapper")
     submenu.classList.toggle("open");
+
 }
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === 'Enter') {
+        toggleAccountMenu();
+    }
+});
+
---
+++ updatedContent/updatedJS/updatedGeneral/toggleNotificationMenu.js
@@ -0,0 +1,12 @@
+function toggleNotificationMenu() {
+    let submenu = document.getElementById("id_notifications_submenu_wrapper")
+    submenu.classList.toggle("open");
+
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === 'Enter') {
+        toggleNotificationMenu();
+    }
+});
+
