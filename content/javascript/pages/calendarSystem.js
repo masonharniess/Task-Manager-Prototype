3669f0a44e255b80eb51c6eca70c510f35d101fb
masonharniess <masonharniess@outlook.com>
2023-04-30 18:46:05 +0100

    in middle of javascript clean, stopping for today

--- content/javascript/pages/calendarSystem.js
+++ content/javascript/pages/calendarSystem.js
@@ -1,11 +1,11 @@
 function calendarSystem() {
-  const days2 = document.querySelector(".calendar_days")
-  const currentDate2 = document.querySelector(".current_date")
-  const previousSubsequentIcon = document.querySelector(".chevron")
-
-  let date2 = new Date();
-  let currentYear = date2.getFullYear();
-  let currentMonth = date2.getMonth();
+  // const days2 = document.querySelector(".calendar_days")
+  // const currentDate2 = document.querySelector(".current_date")
+  // const previousSubsequentIcon = document.querySelector(".chevron")
+  //
+  // let date2 = new Date();
+  // let currentYear = date2.getFullYear();
+  // let currentMonth = date2.getMonth();
 
   const months2 = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
@@ -25,19 +25,18 @@ function calendarSystem() {
     prevNextIcon = document.querySelectorAll(".icons span");
 
 // getting new date, current year and month
-  let date = new Date(),
-    currYear = date.getFullYear(),
-    currMonth = date.getMonth();
+  let date = new Date();
+  let currentYear = date.getFullYear();
+  let currentMonth = date.getMonth();
 
 // storing full name of all months in array
-  const months = ["January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December"];
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
-      lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-      lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
-      lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+    let firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay(); // getting first day of month
+    let lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); // getting last date of month
+    let lastDayOfMonth = new Date(currentYear, currentMonth, lastDateofMonth).getDay(); // getting last day of month
+    let lastDateOfLastMonth = new Date(currentYear, currentMonth, 0).getDate(); // getting last date of previous month
     let liTag = "";
 
     for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
@@ -46,15 +45,15 @@ function calendarSystem() {
 
     for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
       // adding active class to li if the current day, month, and year matched
-      let isToday = i === date.getDate() && currMonth === new Date().getMonth()
-      && currYear === new Date().getFullYear() ? "active" : "";
+      let isToday = i === date.getDate() && currentMonth === new Date().getMonth()
+      && currentYear === new Date().getFullYear() ? "active" : "";
       liTag += `<li class="${isToday}">${i}</li>`;
     }
 
     for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
       liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
     }
-    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+    currentDate.innerText = `${months[currentMonth]} ${currentYear}`; // passing current mon and yr as currentDate text
     daysTag.innerHTML = liTag;
   }
   renderCalendar();
@@ -62,13 +61,13 @@ function calendarSystem() {
   prevNextIcon.forEach(icon => { // getting prev and next icons
     icon.addEventListener("click", () => { // adding click event on both icons
       // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
-      currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+      currentMonth = icon.id === "prev" ? currentMonth - 1 : currentMonth + 1;
 
-      if (currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
+      if (currentMonth < 0 || currentMonth > 11) { // if current month is less than 0 or greater than 11
         // creating a new date of current year & month and pass it as date value
-        date = new Date(currYear, currMonth, new Date().getDate());
-        currYear = date.getFullYear(); // updating current year with new date year
-        currMonth = date.getMonth(); // updating current month with new date month
+        date = new Date(currentYear, currentMonth, new Date().getDate());
+        currentYear = date.getFullYear(); // updating current year with new date year
+        currentMonth = date.getMonth(); // updating current month with new date month
       } else {
         date = new Date(); // pass the current date as date value
       }
