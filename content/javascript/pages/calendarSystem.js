cbb178df9c42928d8a831966f78a4e4dd6e37e93
masonharniess <masonharniess@outlook.com>
2023-05-02 09:38:16 +0100

    fixed capitalisation, old calendar works

--- content/javascript/pages/calendarSystem.js
+++ content/javascript/pages/calendarSystem.js
@@ -35,23 +35,23 @@ function calendarSystem() {
   const renderCalendar = () => {
     let firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay(); // getting first day of month
     let lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); // getting last date of month
-    let lastDayOfMonth = new Date(currentYear, currentMonth, lastDateofMonth).getDay(); // getting last day of month
+    let lastDayOfMonth = new Date(currentYear, currentMonth, lastDateOfMonth).getDay(); // getting last day of month
     let lastDateOfLastMonth = new Date(currentYear, currentMonth, 0).getDate(); // getting last date of previous month
     let liTag = "";
 
-    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
-      liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+    for (let i = firstDayOfMonth; i > 0; i--) { // creating li of previous month last days
+      liTag += `<li class="inactive">${lastDateOfLastMonth - i + 1}</li>`;
     }
 
-    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
+    for (let i = 1; i <= lastDateOfMonth; i++) { // creating li of all days of current month
       // adding active class to li if the current day, month, and year matched
       let isToday = i === date.getDate() && currentMonth === new Date().getMonth()
       && currentYear === new Date().getFullYear() ? "active" : "";
       liTag += `<li class="${isToday}">${i}</li>`;
     }
 
-    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
-      liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+    for (let i = lastDayOfMonth; i < 6; i++) { // creating li of next month first days
+      liTag += `<li class="inactive">${i - lastDayOfMonth + 1}</li>`
     }
     currentDate.innerText = `${months[currentMonth]} ${currentYear}`; // passing current mon and yr as currentDate text
     daysTag.innerHTML = liTag;
